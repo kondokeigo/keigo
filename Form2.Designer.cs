5850af27d7c450abec2e5c4bb26ebcd321c195c7
kondokeigo <88736464+kondokeigo@users.noreply.github.com>
2021-09-04 22:18:08 +0900

    Add files via upload

---
+++ Form2.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace WindowsFormsApp14
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column8,
+            this.Column9,
+            this.Column7});
+            this.dataGridView2.Location = new System.Drawing.Point(-1, 0);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 21;
+            this.dataGridView2.Size = new System.Drawing.Size(1219, 467);
+            this.dataGridView2.TabIndex = 0;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.FillWeight = 103.5503F;
+            this.Column1.HeaderText = "氏名";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 150;
+            // 
+            // Column2
+            // 
+            this.Column2.FillWeight = 100.6039F;
+            this.Column2.HeaderText = "氏名カナ";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 146;
+            // 
+            // Column3
+            // 
+            this.Column3.FillWeight = 39.23732F;
+            this.Column3.HeaderText = "性別";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 57;
+            // 
+            // Column4
+            // 
+            this.Column4.FillWeight = 104.6606F;
+            this.Column4.HeaderText = "大学";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 151;
+            // 
+            // Column5
+            // 
+            this.Column5.FillWeight = 101.55F;
+            this.Column5.HeaderText = "学部";
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 147;
+            // 
+            // Column6
+            // 
+            this.Column6.FillWeight = 39.55978F;
+            this.Column6.HeaderText = "学年";
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 58;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "電話番号";
+            this.Column8.Name = "Column8";
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "メールアドレス";
+            this.Column9.Name = "Column9";
+            // 
+            // Column7
+            // 
+            this.Column7.FillWeight = 210.8381F;
+            this.Column7.HeaderText = "備考";
+            this.Column7.Name = "Column7";
+            this.Column7.Width = 305;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 473);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "保存";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1221, 502);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView2);
+            this.Name = "Form2";
+            this.Text = "参加者一覧";
+            this.Shown += new System.EventHandler(this.Form2_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+    }
+}
